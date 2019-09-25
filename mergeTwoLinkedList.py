2e69da6427e09404c1b23b7067318285fa13bcfc
haminthecoder <jham.lee@mail.utoronto.ca>
2019-09-25 19:15:12 +0900

    merge two linked list solution

---
+++ mergeTwoLinkedList.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # Initialize temp variable to store the head node
+        # and keep track of current node
+        temp = curr = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        
+#         Python's or returns the longer list
+        return temp.next
+        
+            
+     
+            
+        
