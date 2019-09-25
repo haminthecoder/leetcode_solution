f1f9f0923389ab16e850a677ab172a91a5b438c4
haminthecoder <jham.lee@mail.utoronto.ca>
2019-09-25 21:55:54 +0900

    queue.java solution for 1QBit interview

---
+++ queue.java
@@ -0,0 +1,46 @@
+
+class Queue {
+    private int arr[];
+    private int first; 
+   
+    public int size(int arr[]) {
+        return sizeof.arr;
+    }
+    
+    public boolean isEmpty() {
+        if (size(arr) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+   
+    public boolean push(int x) {
+        // Size of the array
+        int index = size();
+        if (index > -1) {
+           arr.append(x);
+           return true;
+        }
+        
+        return false;
+    }
+    
+    public int pop() {
+        if (!isEmpty()) {
+            int temp = arr[0];
+            // pop first element
+            arr = arr.slice(1, size(arr));
+            
+            return temp;
+        } 
+       
+        return -1;
+       
+    }
+  
+    
+    
+
+}
+
