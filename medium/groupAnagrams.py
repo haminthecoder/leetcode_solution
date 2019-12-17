37a1bf5e2e0fae1e90f6c49608d84a2830a17530
haminthecoder <jham.lee@mail.utoronto.ca>
2019-12-17 23:24:51 +0900

    Wordbreak DP solution

--- medium/groupAnagrams.py
+++ medium/groupAnagrams.py
@@ -15,24 +15,25 @@ class Solution:
         return answer
     #   return [ anas[x] for x in anas ]
 
-    # Brute Force
-    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    #     result = []
-    #     for i in range(len(strs)):
-    #         if not result:
-    #             result.append([strs[i]])
-    #             other.append(sorted(strs[i]))
-    #         else:
-    #             flag = False
-    #             for j in range(len(result)):
-    #                 tmp = sorted(result[j][0])
-    #                 if tmp == sorted(strs[i]):
-    #                     result[j].append(strs[i])
-    #                     flag = True
-    #                     break
-    #             if flag == False:
-    #                 result.append([strs[i]])
-    #     return result
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        copy = strs.copy()
+        answer = []
+        d = {}
+        
+        for i in range(len(strs)):
+            tmp = sorted(copy[i])
+            copy[i] = "".join(tmp)
+            
+        for i in range(len(copy)):
+            d[copy[i]] = d.get(copy[i], []) + [strs[i]]
+                    
+        for item in d.values():
+
+            answer.append(item)
+        
+        return answer
+        
     
                 
             
